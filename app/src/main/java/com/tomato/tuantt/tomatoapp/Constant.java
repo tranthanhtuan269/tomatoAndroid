c86672bab615611b046c3b507e37deeb44129841
Anh Nguyen <anhnd@tohsoft.com>
2018-10-11 08:45:34 +0700

    thêm map

---
+++ app/src/main/java/com/tomato/tuantt/tomatoapp/Constant.java
@@ -0,0 +1,8 @@
+package com.tomato.tuantt.tomatoapp;
+
+/**
+ * Created by Anh Nguyen on 10/10/2018.
+ */
+public class Constant {
+    public static final String GEO_KEY="AIzaSyDgNk9HZU9SKl_KvGrEQmI1VCmNwU6ZSn0";
+}
